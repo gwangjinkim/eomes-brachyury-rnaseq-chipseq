49b75ea17165bde1852b59b1efc0fea37e545a59
Gwang-Jin Kim <32566039+gwangjinkim@users.noreply.github.com>
2022-01-17 20:39:42 +0100

    Update fig-1h-alignment.sh
    added original command on cluster

--- fig-1h-alignment.sh
+++ fig-1h-alignment.sh
@@ -26,6 +26,16 @@ rsubread-oct2018-mouse.r "WT-embr-6-2" "-1.fastq.gz" "${base}/data/galaxy_2020_j
 rsubread-oct2018-mouse.r "WT-embr-13-3" "-1.fastq.gz" "${base}/data/galaxy_2020_jan" "${base}/alignment/galaxy_2020_jan" "${base}/count/galaxy_2020_jan" 4 &> ${base}/alignment/galaxy_2020_jan/WT-embr-13-3.log & \
 rsubread-oct2018-mouse.r "WT-embr-14-4" "-1.fastq.gz" "${base}/data/galaxy_2020_jan" "${base}/alignment/galaxy_2020_jan" "${base}/count/galaxy_2020_jan" 4 &> ${base}/alignment/galaxy_2020_jan/WT-embr-14-4.log
 
+# originally actually done in the cluster
+msub /home/fr/fr_fr/fr_gk1029/script/new/rsubread-oct2018-mouse.sh "EoBRA-embr-5-1"
+msub /home/fr/fr_fr/fr_gk1029/script/new/rsubread-oct2018-mouse.sh "EoBRA-embr-8-2"
+msub /home/fr/fr_fr/fr_gk1029/script/new/rsubread-oct2018-mouse.sh "EoBRA-embr-9-3"
+msub /home/fr/fr_fr/fr_gk1029/script/new/rsubread-oct2018-mouse.sh "EoBRA-embr-10-4"
+msub /home/fr/fr_fr/fr_gk1029/script/new/rsubread-oct2018-mouse.sh "WT-embr-1-1"
+msub /home/fr/fr_fr/fr_gk1029/script/new/rsubread-oct2018-mouse.sh "WT-embr-6-2"
+msub /home/fr/fr_fr/fr_gk1029/script/new/rsubread-oct2018-mouse.sh "WT-embr-13-3"
+msub /home/fr/fr_fr/fr_gk1029/script/new/rsubread-oct2018-mouse.sh "WT-embr-14-4"
+
 # count files used for downstream analyses:y
 EoBRA-embr-10-4-sym-fcount.tab
 EoBRA-embr-5-1-sym-fcount.tab
