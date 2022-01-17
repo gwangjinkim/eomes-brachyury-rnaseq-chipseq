82225e9ff6642f368449e5fc2186cc7d9f3e2cd6
Gwang-Jin Kim <32566039+gwangjinkim@users.noreply.github.com>
2022-01-17 17:17:55 +0100

    Create fig2c-g dosntream analysis
    Generation of the heatmap fig2E which was basis for fig2C-D,F-Gand other analyses.

---
+++ fig-1h-downstream-anlaysis.R
@@ -0,0 +1,218 @@
+db = {
+"wtn": ["/media/josephus/archive_big/count/JT-cells-hybrid-new/subread/WTnewrep1-sym-fcount.tab",
+"/media/josephus/archive_big/count/JT-cells-hybrid-new/subread/WTnewrep2-sym-fcount.tab",
+"/media/josephus/archive_big/count/JT-cells-hybrid-new/subread/WTnewrep3-sym-fcount.tab",],
+"EoV5-nodox": ["/media/josephus/archive_big/count/JT-cells-Eo56R/subread/EoV5newActrep1-sym-fcount.tab",
+"/media/josephus/archive_big/count/JT-cells-Eo56R/subread/EoV5newActrep2-sym-fcount.tab",
+"/media/josephus/archive_big/count/JT-cells-Eo56R/subread/EoV5newActrep3-sym-fcount.tab",],
+"EoEnR": ["/media/josephus/archive_big/kworkspace/fr_gk1029-count-0/JT-cells-old/subread/eo-RAD-1-sym-fcount.tab",
+"/media/josephus/archive_big/kworkspace/fr_gk1029-count-0/JT-cells-old/subread/eo-RAD-2-sym-fcount.tab",
+"/media/josephus/archive_big/kworkspace/fr_gk1029-count-0/JT-cells-old/subread/eo-RAD-3-sym-fcount.tab",],
+"EoVP16": ["/media/josephus/archive_big/kworkspace/fr_gk1029-count-0/JT-cells-old/subread/eo-6AD-1-sym-fcount.tab",
+"/media/josephus/archive_big/kworkspace/fr_gk1029-count-0/JT-cells-old/subread/eo-6AD-2-sym-fcount.tab",
+"/media/josephus/archive_big/kworkspace/fr_gk1029-count-0/JT-cells-old/subread/eo-6AD-3-sym-fcount.tab",],
+"EoGFP": ["/media/josephus/archive_big/kworkspace/count/BraChIP2018/rsubread/grcm38/EoGFPdox1-sym-fcount.tab",
+"/media/josephus/archive_big/kworkspace/count/BraChIP2018/rsubread/grcm38/EoGFPdox2-sym-fcount.tab",
+"/media/josephus/archive_big/kworkspace/count/BraChIP2018/rsubread/grcm38/EoGFPdox3-sym-fcount.tab",],
+"EoTBX": ["/media/josephus/archive_big/count/eo_bra_dko_revision_2019/H3FC5BGXC_JT_29072019_19s003327-1-1_Arnold_lane1EoTBXdoxRep1-sym-fcount.tab",
+          "/media/josephus/archive_big/count/eo_bra_dko_revision_2019/H3FC5BGXC_JT_29072019_19s003327-1-1_Arnold_lane1EoTBXdoxRep2-sym-fcount.tab",
+          "/media/josephus/archive_big/count/eo_bra_dko_revision_2019/H3FC5BGXC_JT_29072019_19s003327-1-1_Arnold_lane1EoTBXdoxRep3-sym-fcount.tab",],
+"BraEnR": ["/media/josephus/archive_big/kworkspace/fr_gk1029-count-0/JT-cells-second-new/subread/BraEnRActdoxrep1-sym-fcount.tab",
+"/media/josephus/archive_big/kworkspace/fr_gk1029-count-0/2018_05_eobr6CR/subread/br-RAD-2-sym-fcount.tab",
+"/media/josephus/archive_big/kworkspace/fr_gk1029-count-0/2018_05_eobr6CR/subread/br-RAD-3-sym-fcount.tab",],
+"BraVP16": ["/media/josephus/archive_big/kworkspace/count/2018_05_eobr6CR/subread/br-6AD-1-sym-fcount.tab",
+"/media/josephus/archive_big/kworkspace/count/2018_05_eobr6CR/subread/br-6AD-2-sym-fcount.tab",
+"/media/josephus/archive_big/kworkspace/count/2018_05_eobr6CR/subread/br-6AD-3-sym-fcount.tab",],
+"BraGFP": ["/media/josephus/archive_big/kworkspace/count/BraChIP2018/rsubread/grcm38/BraGFPdox1-sym-fcount.tab",
+"/media/josephus/archive_big/kworkspace/count/BraChIP2018/rsubread/grcm38/BraGFPdox2-sym-fcount.tab",
+"/media/josephus/archive_big/kworkspace/count/BraChIP2018/rsubread/grcm38/BraGFPdox3-sym-fcount.tab",],
+"BraTBX": ["/media/josephus/archive_big/count/eo_bra_dko_revision_2019/H3FC5BGXC_JT_29072019_19s003327-1-1_Arnold_lane1BraTBXdoxRep1-sym-fcount.tab",
+           "/media/josephus/archive_big/count/eo_bra_dko_revision_2019/H3FC5BGXC_JT_29072019_19s003327-1-1_Arnold_lane1BraTBXdoxRep2-sym-fcount.tab",
+           "/media/josephus/archive_big/count/eo_bra_dko_revision_2019/H3FC5BGXC_JT_29072019_19s003327-1-1_Arnold_lane1BraTBXdoxRep3-sym-fcount.tab"],
+"WT-embr": ["/media/josephus/My Book/Downloads/data_jelena/embr/WTembr1.txt",
+"/media/josephus/My Book/Downloads/data_jelena/embr/WTembr2.txt",
+"/media/josephus/My Book/Downloads/data_jelena/embr/WTembr3.txt",
+"/media/josephus/My Book/Downloads/data_jelena/embr/BraKOembr3.txt",],
+"EoKO-embr": ["/media/josephus/My Book/Downloads/data_jelena/embr/EoKOembr1.txt",
+"/media/josephus/My Book/Downloads/data_jelena/embr/EoKOembr2.txt",
+"/media/josephus/My Book/Downloads/data_jelena/embr/EoKOembr3.txt",],
+"EoBraKO-embr": ["/media/josephus/My Book/Downloads/data_jelena/embr/dKOembr1.txt",
+"/media/josephus/My Book/Downloads/data_jelena/embr/dKOembr2.txt",
+"/media/josephus/My Book/Downloads/data_jelena/embr/dKOembr3.txt",],
+"BraKO-embr": ["/media/josephus/My Book/Downloads/data_jelena/embr/BraKOembr1.txt",
+"/media/josephus/My Book/Downloads/data_jelena/embr/BraKOembr2.txt",],
+"EoBraKI-embr": [],
+"EoBraKO": ["/media/josephus/archive_big/count/eo_bra_dko_revision_2019/H3FC5BGXC_JT_29072019_19s003327-1-1_Arnold_lane1dKOdoxRep1-sym-fcount.tab",
+"/media/josephus/archive_big/count/eo_bra_dko_revision_2019/H3FC5BGXC_JT_29072019_19s003327-1-1_Arnold_lane1dKOdoxRep2-sym-fcount.tab",
+"/media/josephus/archive_big/count/eo_bra_dko_revision_2019/H3FC5BGXC_JT_29072019_19s003327-1-1_Arnold_lane1dKOdoxRep3-sym-fcount.tab",],
+"BraKO": ["/media/josephus/archive_big/count/JT-cells-hybrid-new/subread/br-1-sym-fcount.tab",
+"/media/josephus/archive_big/count/JT-cells-hybrid-new/subread/br-2-sym-fcount.tab",
+"/media/josephus/archive_big/count/JT-cells-hybrid-new/subread/br-3-sym-fcount.tab",],
+"EoKO": ["/media/josephus/archive_big/count/JT-cells-hybrid-new/subread/EomesKOnewrep1-sym-fcount.tab",
+"/media/josephus/archive_big/count/JT-cells-hybrid-new/subread/EomesKOnewrep2-sym-fcount.tab",
+"/media/josephus/archive_big/count/JT-cells-hybrid-new/subread/EomesKOnewrep3-sym-fcount.tab",],
+"kidney-280120-wt": ["/media/josephus/archive_big/count/galaxy_2020_jan/kidney-280120-wt1-sym-fcount.tab",
+"/media/josephus/archive_big/count/galaxy_2020_jan/kidney-280120-wt2-sym-fcount.tab",
+"/media/josephus/archive_big/count/galaxy_2020_jan/kidney-280120-wt3-sym-fcount.tab",
+"/media/josephus/archive_big/count/galaxy_2020_jan/kidney-280120-wt4-sym-fcount.tab",
+"/media/josephus/archive_big/count/galaxy_2020_jan/kidney-280120-wt5-sym-fcount.tab",],
+"kidney-280120-wt-mod": ["/media/josephus/archive_big/count/galaxy_2020_jan/kidney-280120-wt1-sym-fcount.tab",
+"/media/josephus/archive_big/count/galaxy_2020_jan/kidney-280120-wt2-sym-fcount.tab",
+"/media/josephus/archive_big/count/galaxy_2020_jan/kidney-280120-wt3-sym-fcount.tab",
+"/media/josephus/archive_big/count/galaxy_2020_jan/kidney-280120-wt5-sym-fcount.tab",],
+"kidney-280120-mut": ["/media/josephus/archive_big/count/galaxy_2020_jan/kidney-280120-mut1-sym-fcount.tab",
+"/media/josephus/archive_big/count/galaxy_2020_jan/kidney-280120-mut2-sym-fcount.tab",
+"/media/josephus/archive_big/count/galaxy_2020_jan/kidney-280120-mut3-sym-fcount.tab",
+"/media/josephus/archive_big/count/galaxy_2020_jan/kidney-280120-mut4-sym-fcount.tab",
+"/media/josephus/archive_big/count/galaxy_2020_jan/kidney-280120-mut5-sym-fcount.tab",],
+"EoBRA-embr": ["/media/josephus/archive_big/count/galaxy_2020_jan/EoBRA-embr-10-4-sym-fcount.tab",
+"/media/josephus/archive_big/count/galaxy_2020_jan/EoBRA-embr-5-1-sym-fcount.tab",
+"/media/josephus/archive_big/count/galaxy_2020_jan/EoBRA-embr-8-2-sym-fcount.tab",
+"/media/josephus/archive_big/count/galaxy_2020_jan/EoBRA-embr-9-3-sym-fcount.tab",],
+"WTn-embr": ["/media/josephus/archive_big/count/galaxy_2020_jan/WT-embr-1-1-sym-fcount.tab",
+"/media/josephus/archive_big/count/galaxy_2020_jan/WT-embr-13-3-sym-fcount.tab",
+"/media/josephus/archive_big/count/galaxy_2020_jan/WT-embr-14-4-sym-fcount.tab",
+"/media/josephus/archive_big/count/galaxy_2020_jan/WT-embr-6-2-sym-fcount.tab",],
+}
+
+k = 12
+
+#############################################
+# write a Python script which creates into meta folder a meta
+#############################################
+
+def meta_lines(key, dct, name=None, testing="", sep="\t"):
+    """Returns meta line.
+    sampleName  fileName        condition       testing
+    testing = ["", "denom", "num"]
+    """
+    if name is None:
+        name = key
+
+    fileNames = dct[key]
+    sampleNames = [ name + "-" + str(i) for i, _ in enumerate(fileNames) ]
+    conditions = [ name for i, _ in enumerate(fileNames) ]
+    testings = [ testing for i, _ in enumerate(fileNames) ]
+    return [ sep.join((sn, fn, cn, ts)) for sn, fn, cn, ts in zip(sampleNames, fileNames, conditions, testings)]
+
+
+import os
+
+def create_meta(keys, dct, meta_dir, project_name, source, sep="\t", names=None):
+    """
+    Automatic creation of meta file.
+    keys: list of keays for dct. Order determines testing!:
+          FIRST ELEMENT is "denom"
+          SECOND ELEMENT is "num"
+          rest is ""
+    dct:  contains key and pathlist to keys
+    meta_dir: the directory where meta path is created.
+          name of the meta file is generated automatically using project_name and source.
+    project_name: inserted as signifier into meta filename.
+    source: inserted as signifier into meta filename.
+            (where cells came from? "embr", "cells", cell line name ...)
+    sep:  separator in meta file - "\t"
+    names: names to be used instead of keys.
+    """
+    if names is None:
+        names = keys
+
+    # automatic file name/path generation:
+
+    fname = "meta_" + project_name + "_" + source + "_" + \
+            names[1] + "-vs-" + names[0] + "_" + \
+            '_'.join(names[2:]) + \
+            ".txt"
+    fpath = os.path.join(meta_dir, fname)
+
+    # automatic file content generation:
+    res = [sep.join(["sampleName", "fileName", "condition", "testing"])]
+    testings = ["denom", "num"] + ["" for _ in keys[2:]]
+    for k, nm, t in zip(keys, names, testings):
+        res.extend(meta_lines(k, dct, nm, t, sep))
+
+    # write down all lines
+    with open(fpath, "w") as out:
+        for line in res:
+            print(line, file=out)
+    print('\n'.join(res))
+    print("")
+    print(f'"{fpath}"')
+    return fpath, names[1] + "-vs-" + names[0] + "_" + "_".join(names[2:])
+
+
+meta_dir = "/media/josephus/Elements/metas"
+
+
+def analyze(keys=["WT-embr", "EoKO-embr", "BraKO-embr"],
+            dct=db,
+            meta_dir=meta_dir,
+            project_name="targets2020",
+            source="embr",
+            sep="\t",
+            names=["WT", "EoKO", "BraKO"],
+            script_fpath="/home/josephus/Dropbox/a_myRNAseq/rdownstream/191004.rdownstream.central.R",
+            out_dir="/media/josephus/Elements/DEanalysis",
+            alpha = 0.05,
+            lFC = 1,
+            ks = "12",
+            go = "skip"):
+    meta_fpath, dirname = create_meta(keys = keys, dct = dct, meta_dir = meta_dir,
+                             project_name = project_name,
+                             source = source, sep = sep, names = names)
+    data_name = project_name + "_" + source
+    cmd = f'/media/josephus/Elements/script/Rscript-analysis20200127.R ' + \
+          f'-s "{script_fpath}" ' + \
+          f'-o "{os.path.join(out_dir, data_name, dirname)}" ' + \
+          f'-m "{meta_fpath}" ' + \
+          f'-d "{data_name}" ' + \
+          f'-a {alpha} ' + \
+          f'-l {lFC} ' + \
+          f'-k "{ks}" ' + \
+          f'-g "{go}"'
+    os.system(cmd)
+
+# analyze() works!
+
+
+# # create_meta(keys=["wtn", "EoV5-nodox", "EoGFP", "BraGFP"],
+# #             dct=db,
+# #             meta_dir=meta_dir,
+# #             project_name="targets2020",
+# #             source="JT-cells",
+# #             sep="\t")
+# # 
+# # # sampleName        fileName        condition       testing
+# # # wtn-0     /media/josephus/archive_big/count/JT-cells-hybrid-new/subread/WTnewrep1-sym-fcount.tab  wtn     denom
+# # # wtn-1     /media/josephus/archive_big/count/JT-cells-hybrid-new/subread/WTnewrep2-sym-fcount.tab  wtn     denom
+# # # wtn-2     /media/josephus/archive_big/count/JT-cells-hybrid-new/subread/WTnewrep3-sym-fcount.tab  wtn     denom
+# # # EoV5-nodox-0      /media/josephus/archive_big/count/JT-cells-Eo56R/subread/EoV5newActrep1-sym-fcount.tab  EoV5-nodox      num
+# # # EoV5-nodox-1      /media/josephus/archive_big/count/JT-cells-Eo56R/subread/EoV5newActrep2-sym-fcount.tab  EoV5-nodox      num
+# # # EoV5-nodox-2      /media/josephus/archive_big/count/JT-cells-Eo56R/subread/EoV5newActrep3-sym-fcount.tab  EoV5-nodox      num
+# # # EoGFP-0   /media/josephus/archive_big/kworkspace/count/BraChIP2018/rsubread/grcm38/EoGFPdox1-sym-fcount.tab       EoGFP   
+# # # EoGFP-1   /media/josephus/archive_big/kworkspace/count/BraChIP2018/rsubread/grcm38/EoGFPdox2-sym-fcount.tab       EoGFP   
+# # # EoGFP-2   /media/josephus/archive_big/kworkspace/count/BraChIP2018/rsubread/grcm38/EoGFPdox3-sym-fcount.tab       EoGFP   
+# # # BraGFP-0  /media/josephus/archive_big/kworkspace/count/BraChIP2018/rsubread/grcm38/BraGFPdox1-sym-fcount.tab      BraGFP  
+# # # BraGFP-1  /media/josephus/archive_big/kworkspace/count/BraChIP2018/rsubread/grcm38/BraGFPdox2-sym-fcount.tab      BraGFP  
+# # # BraGFP-2  /media/josephus/archive_big/kworkspace/count/BraChIP2018/rsubread/grcm38/BraGFPdox3-sym-fcount.tab      BraGFP  
+# # # 
+# # # "/media/josephus/Elements/metas/meta_targets2020_JT-cells_EoV5-nodox-vs-wtn_EoGFP_BraGFP.txt"
+# # # 
+
+
+######################################
+# downstream analysis (with heatmap generation)
+######################################
+
+analyze(keys=["wtn", "EoV5-nodox", "EoGFP", "BraGFP"],
+            dct=db,
+            meta_dir=meta_dir,
+            project_name="target2020",
+            source="JT-cells",
+            sep="\t",
+            script_fpath="/home/josephus/Dropbox/a_myRNAseq/rdownstream/191004.rdownstream.central.R",
+            out_dir="/media/josephus/Elements/DEanalysis",
+            alpha = 0.05,
+            lFC = 1,
+            ks = "12",
+            go = "")
